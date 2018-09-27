959c9e1a66624fcf2e533bcba79427e0d0d9e4d6
JulepSinclair <42009732+JulepSinclair@users.noreply.github.com>
2018-09-27 14:38:03 -0400

    Onclick

--- crypto-dash/src/App.js
+++ crypto-dash/src/App.js
@@ -1,35 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components'
+import styled, {css} from 'styled-components';
+
 const Logo = styled.div`
 font-size: 1.5em;
 `
 const ControlButton = styled.div`
+cursor: pointer;
+${props => props.active && css`
+text-shadow: 10px 10px 60px #03ff03
+`}
 `
 
-const AppLayout = styled.div`
+const Bar = styled.div`
 padding: 40px;
+margin-bottom: 40px;
 display: grid;
 grid-template-columns: 180px auto 100px 100px;
 `
 
 class App extends Component {
+
+  state={
+    page: 'dashboard'
+  }
+  displayingDashboard = () => this.state.page === 'dashboard'
+  displayingSettings = () => this.state.page === 'settings'
   render() {
     return (
-      <AppLayout>
+     
+      <div>
+      <Bar>
       <Logo>
       CryptoDash 
       </Logo>
       <div>
       </div>
-      <ControlButton>
+      <ControlButton onClick={()=>{this.setState({page: 'dashboard'})}} active={this.displayingDashboard()}>
       DashBoard
       </ControlButton>
-      <ControlButton>
+      <ControlButton onClick={()=>{this.setState({page: 'settings'})}}active={this.displayingSettings()}>
       Settings
       </ControlButton>
-    
-      </AppLayout>
+      </Bar>
+      <content>
+      Hello I'm {this.state.page}
+      </content>
+      </div>
+     
     );
   }
 }
