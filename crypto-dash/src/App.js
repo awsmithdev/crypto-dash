8043490f51952b8e46caaff6169505b54fc4c7db
JulepSinclair <42009732+JulepSinclair@users.noreply.github.com>
2018-09-27 12:11:39 -0400

    Implemented More Css and App Structure with Appbar

--- crypto-dash/src/App.js
+++ crypto-dash/src/App.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components'
-const CustomElement = styled.div`
-font-size:30px;
+const Logo = styled.div`
+font-size: 1.5em;
+`
+const ControlButton = styled.div`
+`
+
+const AppLayout = styled.div`
+padding: 40px;
+display: grid;
+grid-template-columns: 180px auto 100px 100px;
 `
 
 class App extends Component {
   render() {
     return (
-      <CustomElement>
-      Hello!
-      </CustomElement>
+      <AppLayout>
+      <Logo>
+      CryptoDash 
+      </Logo>
+      <div>
+      </div>
+      <ControlButton>
+      DashBoard
+      </ControlButton>
+      <ControlButton>
+      Settings
+      </ControlButton>
+    
+      </AppLayout>
     );
   }
 }
